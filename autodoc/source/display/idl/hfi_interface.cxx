a0bd05b7b9a8221729016f9b1f6549ab339181c3
Rüdiger Timm <rt@openoffice.org>
2004-07-12 14:26:11 +0000

    INTEGRATION: CWS adc8 (1.2.66); FILE MERGED 2004/07/07 08:42:42 np 1.2.66.3: #i26261# 2004/07/06 14:04:27 np 1.2.66.2: #i25608# 2004/07/02 11:04:11 np 1.2.66.1: #i26261#

--- autodoc/source/display/idl/hfi_interface.cxx
+++ autodoc/source/display/idl/hfi_interface.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: hfi_interface.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-18 14:11:37 $
+ *  last change: $Author: rt $ $Date: 2004-07-12 15:26:11 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -72,9 +72,12 @@
 #include <toolkit/hf_linachain.hxx>
 #include <toolkit/hf_navi_sub.hxx>
 #include <toolkit/hf_title.hxx>
+#include "hfi_doc.hxx"
+#include "hfi_hierarchy.hxx"
 #include "hfi_method.hxx"
 #include "hfi_navibar.hxx"
 #include "hfi_property.hxx"
+#include "hfi_tag.hxx"
 #include "hfi_typetext.hxx"
 #include "hi_linkhelper.hxx"
 
@@ -86,13 +89,9 @@ namespace
 {
 
 const String
-    C_sBaseInterface("Base Hierarchy");
-//const String
-//    C_sList_ExportingServices("Known Services which Export this Interface");
-//const String
-//    C_sList_ExportingServices_Link("Exporting Services");
-//const String
-//    C_sList_ExportingServices_Label("ExportingServices");
+    C_sBaseInterface("Base Interfaces");
+const String
+    C_sList_BaseComments("Comments on Base Interfaces");
 const String
     C_sList_Methods("Methods' Summary");
 const String
@@ -111,21 +110,21 @@ const String
 const String
     C_sList_AttributesDetails_Label("AttributesDetails");
 
-          
-                         
+
+
 enum E_SubListIndices
 {
     sli_Methods = 0,
     sli_MethodDetails = 1,
-    sli_AttributesSummary = 2, 
+    sli_AttributesSummary = 2,
     sli_AttributesDetails = 3
 };
 
 } //anonymous namespace
-                         
-                         
-                         
-                         
+
+
+
+
 HF_IdlInterface::HF_IdlInterface( Environment &         io_rEnv,
                                   Xml::Element &        o_rOut )
     :   HtmlFactory_Idl(io_rEnv, &o_rOut),
@@ -135,7 +134,7 @@ HF_IdlInterface::HF_IdlInterface( Environment &         io_rEnv,
 
 HF_IdlInterface::~HF_IdlInterface()
 {
-}                     
+}
 
 void
 HF_IdlInterface::Produce_byData( const client & i_ce ) const
@@ -144,28 +143,31 @@ HF_IdlInterface::Produce_byData( const client & i_ce ) const
         pNaviSubRow( &make_Navibar(i_ce) );
 
     HF_TitleTable
-        aTitle(CurOut());   
-        
+        aTitle(CurOut());
+
     HF_LinkedNameChain
         aNameChain(aTitle.Add_Row());
     aNameChain.Produce_CompleteChain(Env().CurPosition(), nameChainLinker);
 
-    aTitle.Produce_Title( StreamLock(200)() << C_sCePrefix_Interface 
-                                         << " " 
-                                         << i_ce.LocalName() 
+    aTitle.Produce_Title( StreamLock(200)() << C_sCePrefix_Interface
+                                         << " "
+                                         << i_ce.LocalName()
                                          << c_str );
-    produce_Bases(aTitle.Add_Row(),i_ce,C_sBaseInterface);
+
+    produce_BaseHierarchy( aTitle.Add_Row(),
+                           i_ce,
+                           C_sBaseInterface );
 
     write_Docu(aTitle.Add_Row(), i_ce);
     CurOut() << new Html::HorizontalLine();
-                      
+
     dyn_ce_list dpFunctions;
     ary::idl::ifc_interface::attr::Get_Functions(dpFunctions, i_ce);
-    
-    if ( BOOL_OF(*dpFunctions) )
-    {                           
-        eCurProducedMembers = mem_Functions;    
-        
+
+    if ( (*dpFunctions).operator bool() )
+    {
+        eCurProducedMembers = mem_Functions;
+
         produce_Members( *dpFunctions,
                          C_sList_Methods,
                          C_sList_Methods_Label,
@@ -175,13 +177,13 @@ HF_IdlInterface::Produce_byData( const client & i_ce ) const
         pNaviSubRow->SwitchOn(sli_MethodDetails);
     }
 
-    dyn_ce_list 
+    dyn_ce_list
         dpAttributes;
     ary::idl::ifc_interface::attr::Get_Attributes(dpAttributes, i_ce);
-    if ( BOOL_OF(*dpAttributes) )
-    {               
+    if ( (*dpAttributes).operator bool() )
+    {
         eCurProducedMembers = mem_Attributes;
-        
+
         produce_Members( *dpAttributes,
                          C_sList_Attributes,
                          C_sList_Attributes_Label,
@@ -193,12 +195,6 @@ HF_IdlInterface::Produce_byData( const client & i_ce ) const
     eCurProducedMembers = mem_none;
 
     pNaviSubRow->Produce_Row();
-}
-
-HtmlFactory_Idl::type_id
-HF_IdlInterface::inq_BaseOf( const client & i_ce ) const
-{
-    return ary::idl::ifc_interface::attr::Base(i_ce);
 }
 
 DYN HF_NaviSubRow &
@@ -219,12 +215,12 @@ HF_IdlInterface::make_Navibar( const client & i_ce ) const
     return ret;
 }
 
-void                
+void
 HF_IdlInterface::produce_MemberDetails( HF_SubTitleTable &  o_table,
                                         const client &      i_ce ) const
-{                        
+{
     switch (eCurProducedMembers)
-    {                      
+    {
         case mem_Functions:
                     break;
         case mem_Attributes:
@@ -241,16 +237,16 @@ HF_IdlInterface::produce_MemberDetails( HF_SubTitleTable &  o_table,
     typedef ary::idl::ifc_function::attr    funcAttr;
 
     HF_IdlMethod
-        aFunction(  Env(), 
+        aFunction(  Env(),
                     o_table.Add_Row()
                         >> *new Html::TableCell
                            << new Html::ClassAttr(C_sCellStyle_MDetail) );
 
-    ary::Dyn_StdConstIterator<ary::idl::Parameter> 
+    ary::Dyn_StdConstIterator<ary::idl::Parameter>
         pParameters;
     funcAttr::Get_Parameters(pParameters, i_ce);
 
-    ary::Dyn_StdConstIterator<ary::idl::Type_id> 
+    ary::Dyn_StdConstIterator<ary::idl::Type_id>
         pExceptions;
     funcAttr::Get_Exceptions(pExceptions, i_ce);
 
@@ -259,5 +255,102 @@ HF_IdlInterface::produce_MemberDetails( HF_SubTitleTable &  o_table,
                               *pParameters,
                               *pExceptions,
                               funcAttr::IsOneway(i_ce),
+                              funcAttr::HasEllipse(i_ce),
                               i_ce );
+}
+
+void
+HF_IdlInterface::produce_BaseHierarchy( Xml::Element &      o_screen,
+                                        const client &      i_ce,
+                                        const String &      i_sLabel ) const
+{
+    ary::Dyn_StdConstIterator<ary::idl::CommentedRelation>
+        pHelp;
+    ary::idl::ifc_interface::attr::Get_Bases(pHelp, i_ce);
+    if (NOT (*pHelp).operator bool())
+        return;
+
+    // Write hierarchy:
+    HF_IdlBaseNode
+        aMyNode(i_ce, Env().Gate());
+
+    HF_DocEntryList
+        aDocList( o_screen );
+    aDocList.Produce_Term(i_sLabel);
+    Xml::Element &
+        rBaseList = aDocList.Produce_Definition();
+
+    aMyNode.WriteBaseHierarchy(rBaseList, *this, i_ce.LocalName());
+
+
+    // Write comments:
+        // KORR_FUTURE: Make sure, no empty table is constructed when comments list is empty.
+    HF_SubTitleTable
+        aBaseTable( aDocList.Produce_Definition(),
+                "",
+                C_sList_BaseComments,
+                2,
+                HF_SubTitleTable::sublevel_3 );
+
+    for ( ary::StdConstIterator<ary::idl::CommentedRelation> & it = *pHelp;
+          it.operator bool();
+          ++it )
+    {
+        Xml::Element &
+            rRow = aBaseTable.Add_Row();
+
+        Xml::Element &
+            rTerm = rRow
+                        >> *new Html::TableCell
+                            << new Html::ClassAttr(C_sCellStyle_SummaryLeft);
+        HF_IdlTypeText
+            aTypeDisplay( Env(), rTerm, false, 0);
+        aTypeDisplay.Produce_byData((*it).Type());
+
+        Xml::Element &
+            rDocu = rRow
+                        >> *new Html::TableCell
+                            << new Html::ClassAttr(C_sCellStyle_SummaryRight);
+
+        HF_DocEntryList
+            aDocuList(rDocu);
+
+        if ((*it).Info() != 0)
+        {
+//          aDocuList.Produce_Term("Comment on Base Reference");
+
+            HF_IdlDocu
+                aDocuDisplay(Env(), aDocuList);
+            aDocuDisplay.Produce_byDocu4Reference(*(*it).Info(), i_ce);
+        }
+        else
+        {
+            const client *
+                pCe = Env().Linker().Search_CeFromType((*it).Type());
+            const ce_info *
+                pShort = pCe != 0
+                            ?   pCe->Docu()
+                            :   0;
+            if ( pShort != 0 )
+            {
+                aDocuList.Produce_NormalTerm("(referenced interface's summary:)");
+
+                Xml::Element &
+                    rDef = aDocuList.Produce_Definition();
+                HF_IdlDocuTextDisplay
+                    aShortDisplay( Env(), &rDocu, *pCe);
+                pShort->Short().DisplayAt(aShortDisplay);
+            }   // end if (pShort != 0)
+        }   // endif ( (*i_commentedRef).Info() != 0 ) else
+    }   // end for
+}
+
+void
+HF_IdlInterface::Display_BaseNode( const HF_IdlBaseNode & i_rNode ) const
+{
+    // KORR: Check if Env().CurPageCe() is really always the right one
+    //  (probably works).
+    HF_IdlTypeText
+        aDisplay( Env(), CurOut(), true, Env().CurPageCe());
+    aDisplay.Produce_byData(i_rNode.Type());
 }
