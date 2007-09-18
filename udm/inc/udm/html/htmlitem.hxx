7028e5094a24d235710c0a6fbf7e0237afe287cc
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 13:29:36 +0000

    INTEGRATION: CWS adc17 (1.5.26); FILE MERGED 2007/08/10 10:44:54 np 1.5.26.1: #i23626 correct spacing in autodoc output#

--- udm/inc/udm/html/htmlitem.hxx
+++ udm/inc/udm/html/htmlitem.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: htmlitem.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:33:24 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 14:29:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -110,8 +110,6 @@ class Label : public csi::xml::AnElement
                             const ::csv::String & i_sLabel )
                                 : 	csi::xml::AnElement("a")
                                 { *this << new csi::xml::AnAttribute(String("name"), i_sLabel); }
-  private:
-    virtual bool		LineBreakAfterEndTag() const;
 };
 
 /**	<p>
@@ -254,7 +252,7 @@ class Table : public csi::xml::AnElement
                             const ::csv::String & i_sCellSpacing  );
     TableRow &			AddRow();
   private:
-    virtual bool		FinishEmptyTag_XmlStyle() const;  
+    virtual bool		FinishEmptyTag_XmlStyle() const;
     virtual bool		LineBreakAfterBeginTag() const;
 };
 
@@ -302,7 +300,7 @@ class ListItem : public csi::xml::AnElement
                         ListItem()
                                 : 	csi::xml::AnElement("li") {}
   private:
-    virtual bool		LineBreakAfterEndTag() const;	
+    virtual bool		LineBreakAfterEndTag() const;
 };
 
 /** <OL>
